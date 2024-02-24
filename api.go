fc962e844d49404c733ea5d3b9c88ca4a37eb467
Sahildhargave <sahildhargave5253.com>
2024-02-24 11:59:34 -0800

    add

---
+++ api.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type apiFunc func(http.ResponseWriter,*http.Request) error
+
+func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if err := f(w,r); 
+		err != nil {
+			// handle the error
+		}
+	}
+}
+type APIServer struct {
+	listenAddr string
+}
+
+func NewAPIServer(listenAddr string) *APIServer {
+	return &APIServer{
+		listenAddr: listenAddr,
+	}
+}
+
+
+func (s *APIServer) Run(){
+	router := mux.NewRouter()
+	router.HandleFunc("/account", s.handleAccount).Methods("POST")
+
+}
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Response) error{
+return nil
+}
+
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Response) error{
+return nil
+}
+
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Response) error{
+return nil
+}
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Response) error{
+return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Response) error{
+return nil
+}
