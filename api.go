13b83c43bd253616e83a14ce4f6d4eafd242d54a
Sahildhargave <sahildhargave5253.com>
2024-02-24 12:21:36 -0800

    correct thing after geting error with chatgtp

--- api.go
+++ api.go
@@ -1,23 +1,34 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, v any) error{
-	
+type ApiError struct {
+	Error string `json:"error"`
 }
-type apiFunc func(http.ResponseWriter,*http.Request) error
 
-func makeHTTPHandlerFunc(f apiFunc) http.HandlerFunc{
-	return func(w http.ResponseWriter, r *http.Request){
-		if err := f(w,r); 
-		err != nil {
+func WriteJSON(w http.ResponseWriter, status int, v interface{}) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+type apiFunc func(http.ResponseWriter, *http.Request) error
+
+func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
 			// handle the error
+			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
 		}
 	}
 }
+
 type APIServer struct {
 	listenAddr string
 }
@@ -28,28 +39,32 @@ func NewAPIServer(listenAddr string) *APIServer {
 	}
 }
 
-
-func (s *APIServer) Run(){
+func (s *APIServer) Run() {
 	router := mux.NewRouter()
-	router.HandleFunc("/account", s.handleAccount).Methods("POST")
+	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount)).Methods("GET")
+	// Add other routes and handlers here
+	log.Println("JSON API server running on port:", s.listenAddr)
 
-}
-func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Response) error{
-return nil
-}
-
-func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Response) error{
-return nil
+	http.ListenAndServe(s.listenAddr, router)
 }
 
-func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Response) error{
-return nil
+func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
 
-func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Response) error{
-return nil
+func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
 
-func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Response) error{
-return nil
+func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
 }
+
+func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
